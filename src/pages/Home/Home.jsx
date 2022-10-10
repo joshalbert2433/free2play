08562facd0784b9e6ed89dca7a5b35256141b3f6
joshalbert2433 <joshua.albert.villanueva@gmail.com>
2022-10-11 00:02:05 +0800

    add side section content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavBar } from "../../components";
 import "./Home.styled.css";
+import { WindowsLogo } from "phosphor-react";
 
 function Home() {
     return (
@@ -29,7 +30,79 @@ function Home() {
                                     <span>shooter</span>
                                 </div>
                             </div>
+                            <div className="home_game_platform">
+                                <WindowsLogo size={32} />
+                            </div>
                         </div>
+                        <div className="home_card_container">
+                            <img
+                                className="home_game_thumbnail"
+                                src="https://cdn.cloudflare.steamstatic.com/steam/apps/570/capsule_616x353.jpg?t=1662588548"
+                                alt="game"
+                            />
+                            <div className="home_game_details">
+                                <a href="/" className="home_game_title">
+                                    Dota 2
+                                </a>
+                                <p className="home_game_description">
+                                    A hero-focused first-person team shooter
+                                    from Blizzard Entertainment.
+                                </p>
+                                <div className="home_game_tags">
+                                    <span>shooter</span>
+                                </div>
+                            </div>
+                            <div className="home_game_platform">
+                                <WindowsLogo size={32} />
+                            </div>
+                        </div>
+                        <div className="home_card_container">
+                            <img
+                                className="home_game_thumbnail"
+                                src="https://cdn.cloudflare.steamstatic.com/steam/apps/570/capsule_616x353.jpg?t=1662588548"
+                                alt="game"
+                            />
+                            <div className="home_game_details">
+                                <a href="/" className="home_game_title">
+                                    Dota 2
+                                </a>
+                                <p className="home_game_description">
+                                    A hero-focused first-person team shooter
+                                    from Blizzard Entertainment.
+                                </p>
+                                <div className="home_game_tags">
+                                    <span>shooter</span>
+                                </div>
+                            </div>
+                            <div className="home_game_platform">
+                                <WindowsLogo size={32} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="most_played_container">
+                    <h2>Most Played</h2>
+                    <div className="most_played_card">
+                        <img
+                            className="most_played_image"
+                            src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota2_social.jpg"
+                            alt="game"
+                        />
+                        <img
+                            className="most_played_image"
+                            src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota2_social.jpg"
+                            alt="game"
+                        />
+                        <img
+                            className="most_played_image"
+                            src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota2_social.jpg"
+                            alt="game"
+                        />
+                        <img
+                            className="most_played_image"
+                            src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota2_social.jpg"
+                            alt="game"
+                        />
                     </div>
                 </div>
             </section>
